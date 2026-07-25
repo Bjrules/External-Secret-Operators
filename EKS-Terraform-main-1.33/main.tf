e5fe7ba54aa72132bc07f0c62364526ef4878215
Banjo Babade <rulesxx@gmail.com>
2026-07-25 21:09:42 +0100

    Update main.tf

--- EKS-Terraform-main-1.33/main.tf
+++ EKS-Terraform-main-1.33/main.tf
@@ -97,16 +97,6 @@ resource "aws_eks_cluster" "bnj" {
 }
 ############################# With Claude Contribution ############################
 
-data "tls_certificate" "eks" {
-  url = aws_eks_cluster.bnj.identity[0].oidc[0].issuer
-}
-
-resource "aws_iam_openid_connect_provider" "eks" {
-  client_id_list  = ["sts.amazonaws.com"]
-  thumbprint_list = [data.tls_certificate.eks.certificates[0].sha1_fingerprint]
-  url             = aws_eks_cluster.bnj.identity[0].oidc[0].issuer
-}
-
 data "aws_iam_openid_connect_provider" "eks" {
   url = aws_eks_cluster.bnj.identity[0].oidc[0].issuer
 }
