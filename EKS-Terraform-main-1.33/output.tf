b21b2ad083204b9bc1848a40af559a06ca53a6bf
Banjo Babade <rulesxx@gmail.com>
2026-07-25 20:46:32 +0100

    Update output.tf

--- EKS-Terraform-main-1.33/output.tf
+++ EKS-Terraform-main-1.33/output.tf
@@ -1,49 +1,15 @@
-output "region" {
-  description = "AWS region"
-  value       = var.region
+output "cluster_id" {
+  value = aws_eks_cluster.bnj.id
 }
 
-output "cluster_name" {
-  description = "EKS cluster name"
-  value       = module.eks.cluster_name
-}
-
-output "cluster_arn" {
-  description = "Cluster ARN"
-  value       = module.eks.cluster_arn
-}
-
-output "cluster_endpoint" {
-  description = "API endpoint"
-  value       = module.eks.cluster_endpoint
-}
-
-output "cluster_security_group_id" {
-  description = "Cluster SG"
-  value       = module.eks.cluster_security_group_id
-}
-
-output "node_security_group_id" {
-  description = "Node SG"
-  value       = module.eks.node_security_group_id
+output "node_group_id" {
+  value = aws_eks_node_group.bnj.id
 }
 
 output "vpc_id" {
-  description = "VPC ID"
-  value       = module.vpc.vpc_id
+  value = aws_vpc.bnj_vpc.id
 }
 
-output "private_subnets" {
-  description = "Private subnet IDs"
-  value       = module.vpc.private_subnets
-}
-
-output "public_subnets" {
-  description = "Public subnet IDs"
-  value       = module.vpc.public_subnets
-}
-
-output "kubeconfig_cmd" {
-  description = "Command to set kubeconfig"
-  value       = "aws eks update-kubeconfig --name ${module.eks.cluster_name} --region ${var.region}"
+output "subnet_ids" {
+  value = aws_subnet.bnj_subnet[*].id
 }
