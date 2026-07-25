6753718ce399d522acf58b107aea24fd27183174
Banjo Babade <rulesxx@gmail.com>
2026-07-25 01:38:52 +0100

    Update

---
+++ Mega-Project-Terraform-main/output.tf
@@ -0,0 +1,15 @@
+output "cluster_id" {
+  value = aws_eks_cluster.bj.id
+}
+
+output "node_group_id" {
+  value = aws_eks_node_group.bj.id
+}
+
+output "vpc_id" {
+  value = aws_vpc.bj_vpc.id
+}
+
+output "subnet_ids" {
+  value = aws_subnet.bj_subnet[*].id
+}
